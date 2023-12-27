9d91d21749a9c96586fad461e44e7eedec16fa07
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2023-12-27 16:34:31 +0300

    fix

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -12,7 +12,7 @@ function isElementInViewport(el) {
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
